1b8e1c26f97b0b6844da0658d97fdf82ad42d0ae
Sharon Clapp <sbclapp@gmail.com>
2018-01-14 15:43:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function happyHolidayTo(holiday, name) {
 happyHolidayTo('Independence Day', 'you')
 
 function holidayCountdown(holiday, days) {
-  return 'It\'s ${days} days until ${holiday}!"'
+  return `It's ${days} days until ${holiday}!`
 }
 
 holidayCountdown("Mother's Day", 20)
